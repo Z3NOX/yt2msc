f51eaea881d802733e9521adc6cb9f14f749b196
Z3NOX <Z3NOX@users.noreply.github.com>
2022-09-18 20:41:58 +0200

    code cleaned: replaced tabs with whitespaces

--- yt2msc.sh
+++ yt2msc.sh
@@ -29,8 +29,8 @@ USELOCALE=false
 while getopts ":i:c:sl:u" o; do
     case "${o}" in
         i)
-	    INPUTFILE=${OPTARG}
-	    INTERACTIVE=false
+            INPUTFILE=${OPTARG}
+            INTERACTIVE=false
             if [ ! -f "${INPUTFILE}" ]; then
                 echo "input list ${INPUTFILE} does not exist."
                 usage
@@ -39,18 +39,18 @@ while getopts ":i:c:sl:u" o; do
         c)
             CONVERT=${OPTARG}
             ;;
-	s)
-	    # OK if not defined before
-	    SANE=true
-	    ;;
-	l)
-	    LOGFILE=${OPTARG}
-	    ;;
-	u)
-	    USELOCALE=true
-	    ;;
+        s)
+            # OK if not defined before
+            SANE=true
+            ;;
+        l)
+            LOGFILE=${OPTARG}
+            ;;
+        u)
+            USELOCALE=true
+            ;;
         *)
-	    echo "wrong parameter usage..."
+            echo "wrong parameter usage..."
             usage
             ;;
     esac
@@ -96,13 +96,13 @@ if $USELOCALE; then
     mkdir -p "./.bin"
     if [ ! -f "./.bin/yt-dlp" ]; then
         echo "Downloading yt-dlp for the first time"
-	update_ytdl
+        update_ytdl
     fi
     hash -p ${BASEDIR}/.bin/yt-dlp yt-dlp
     updatable=$(yt-dlp -U)
     if ! echo $updatable | grep "yt-dlp is up to date"; then
         echo "Update is needed for yt-dlp"
-	update_ytdl
+        update_ytdl
     fi
         
 fi
@@ -115,11 +115,11 @@ youtube_dl(){
     # --metadata-from-title "(?P<artist>.+?) - (?P<track>.+)[^\(\[]*[\(\[]+[^\)\]]*[\)\]]+" \
     # --embed-thumbnail - only supported by mp3 mp4
     yt-dlp -f 251/249/bestaudio \
-	       -x ${CONVERT:+ --audio-format "${CONVERT}"} \
-	       -o "${OUTPUT}" \
-	       ${SANE:+ --restrict-filenames} \
-	       --add-metadata \
-	       ${@}
+               -x ${CONVERT:+ --audio-format "${CONVERT}"} \
+               -o "${OUTPUT}" \
+               ${SANE:+ --restrict-filenames} \
+               --add-metadata \
+               ${@}
 }
 download(){
     # $1 - url or ID parsable by yt-dlp
@@ -132,62 +132,62 @@ download(){
     chapters=$(cat "${json}" | jq ".chapters")
 
     if [ "${typee}" == "\"playlist\"" ]; then
-	playlist=$(cat "${json}" | jq -r ".title")
-	OUTPUT="${playlist}/%(playlist_index)s - %(artist)s - %(track)s.%(ext)s"
-	# readarray vids < <(cat ${json} | jq ".entries[] | .id" | tr -d "\"")
-	# for i in ${vids[@]}; do
-	#     OUTPUT="${playlist}/"
-	#     echo "download $i"
-	#     download $i
-	# done
-	# exit
+        playlist=$(cat "${json}" | jq -r ".title")
+        OUTPUT="${playlist}/%(playlist_index)s - %(artist)s - %(track)s.%(ext)s"
+        # readarray vids < <(cat ${json} | jq ".entries[] | .id" | tr -d "\"")
+        # for i in ${vids[@]}; do
+        #     OUTPUT="${playlist}/"
+        #     echo "download $i"
+        #     download $i
+        # done
+        # exit
     elif [ "${artist}" == "null" ] || [ "${track}" == "null" ]; then
-	OUTPUT="${OUTPUT}%(title)s.%(ext)s"
+        OUTPUT="${OUTPUT}%(title)s.%(ext)s"
     else
-	OUTPUT="${OUTPUT}%(artist)s - %(track)s.%(ext)s"
+        OUTPUT="${OUTPUT}%(artist)s - %(track)s.%(ext)s"
     fi
-	
+        
     youtube_dl "${1}"
 
     if [ ! -z $LOGFILE ] ; then
-	title=$(yt-dlp --get-title ${1})
-	echo "$1 (${title//$'\n'/\; })" >> ${LOGFILE}
+        title=$(yt-dlp --get-title ${1})
+        echo "$1 (${title//$'\n'/\; })" >> ${LOGFILE}
     fi
 
     # postprocessing
     # echo "DEBUG: $chapters"
     if [ "${chapters}" != "null" ]; then
-	read -p "Chapters found! I am going to split them all! Continue (Y/n)?" choice
+        read -p "Chapters found! I am going to split them all! Continue (Y/n)?" choice
         case "$choice" in
           n|N ) exit ;;
           * )   ;;
         esac
-	fname=$(youtube_dl --get-filename "$1")
-	basefile=$(basename "${fname%.*}")	
-	ext=$(echo "$basefile".*) # expand to written file
-	ext=${ext##*.}          # only take extension
-	echo "DEBUG: \"${basefile}\", \"${ext}\""
+        fname=$(youtube_dl --get-filename "$1")
+        basefile=$(basename "${fname%.*}")        
+        ext=$(echo "$basefile".*) # expand to written file
+        ext=${ext##*.}          # only take extension
+        echo "DEBUG: \"${basefile}\", \"${ext}\""
 
-	mkdir -p "${basefile}"
-	i=0
-	while true; do
-	    title=$(cat "${json}" | jq -r ".chapters[${i}] | .title")
-           fname_i=$(echo ${title} |  tr -d '[:cntrl:][=/=][=$=][=!=][=?=]')
-	    tstart=$(cat "${json}" | jq -r ".chapters[$i] | .start_time")
-	    tend=$(cat "${json}" | jq -r ".chapters[$i] | .end_time")
-	    if [ "${tstart}" == "null" ]; then
-		break
-	    fi
-	    ffmpeg -i "${basefile}.${ext}" \
-		-loglevel fatal \
-		-vcodec copy \
-		-acodec copy \
-		-metadata:s:a:0 TITLE="${title}" \
-		-ss $(date --utc --date "1970-01-01 ${tstart} sec" "+%T") \
-		-to $(date --utc --date "1970-01-01 ${tend} sec" "+%T") \
-               "${basefile}/$(printf "%02.0f" $i) - ${fname_i}.${ext}"
-	    i=$(($i + 1))
-	done
+        mkdir -p "${basefile}"
+        i=0
+        while true; do
+            title=$(cat "${json}" | jq -r ".chapters[${i}] | .title")
+            fname_i=$(echo ${title} |  tr -d '[:cntrl:][=/=][=$=][=!=][=?=]')
+            tstart=$(cat "${json}" | jq -r ".chapters[$i] | .start_time")
+            tend=$(cat "${json}" | jq -r ".chapters[$i] | .end_time")
+            if [ "${tstart}" == "null" ]; then
+                break
+            fi
+            ffmpeg -i "${basefile}.${ext}" \
+                -loglevel fatal \
+                -vcodec copy \
+                -acodec copy \
+                -metadata:s:a:0 TITLE="${title}" \
+                -ss $(date --utc --date "1970-01-01 ${tstart} sec" "+%T") \
+                -to $(date --utc --date "1970-01-01 ${tend} sec" "+%T") \
+                "${basefile}/$(printf "%02.0f" $i) - ${fname_i}.${ext}"
+            i=$(($i + 1))
+        done
     fi
 }
 
@@ -213,10 +213,10 @@ echo "${INPUTFILE}"
 if [ -f ${INPUTFILE} ]; then
     cp "${INPUTFILE}" "${INPUTFILE}.tmp"
     while read line; do
-	if [ ${#line} -le 2 ]; then
-	    continue
-	fi
-	OUTPUT=""
+        if [ ${#line} -le 2 ]; then
+            continue
+        fi
+        OUTPUT=""
         download ${line}
         sed -i "1d" "${INPUTFILE}.tmp"
     done < "${INPUTFILE}"
